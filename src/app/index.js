fe975511dc8118af8341e1da5ff5ac956846f548
CHRISTOPHER SEAMON <chris@28solutions.om>
2018-08-11 13:04:38 -0400

    added .babelrc presets and updated a few items in configuration

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-console.log('it works');
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <p>You're ready to start creating</p>
+            </div>
+        );
+    }
+}
+
+ReactDom.render(<App/>, window.document.getElementById('app'));
