e6414940b4cde70d2f01563084f7894bc5783218
CHRISTOPHER SEAMON <chris@28solutions.om>
2018-08-11 12:24:12 -0400

    initial commit of my configuration

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+var DIST_DIR = path.resolve(__dirname, 'dist');
+var SRC_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+    mode: 'none',
+    entry: SRC_DIR + '/app/index.js',
+    output: {
+        path: DIST_DIR + '/app',
+        filename: 'bundle.js',
+        publicPath: '/app/'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    }
+                ]
+            }
+        ]
+    }
+};
+
+module.exports = config;
